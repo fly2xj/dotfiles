2eed8f1a703c16a7323f512148345d4f53165996
Jean <fly2xj@msn.com>
2013-04-23 13:51:30 +0800

    dircolor

---
+++ dircolor.sh
@@ -0,0 +1,8 @@
+#solarized theme for GNU ls
+git clone git://github.com/seebi/dircolors-solarized.git
+
+cp ~/dircolors-solarized/dircolors.256dark ~/.dircolors
+eval 'dircolors .dircolors'
+
+#Gnome-Terminal
+git clone https://github.com/coolwanglu/gnome-terminal-colors-solarized
